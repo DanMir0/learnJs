aa9291a863983bec64e348f2e12d2cc78a8527f6
DanMir0 <95764144+DanMir0@users.noreply.github.com>
2024-01-05 18:38:34 +0300

    add notification & server

--- login/src/js/app.js
+++ login/src/js/app.js
@@ -4,29 +4,36 @@ import '../css/style.css';
 import UI from './config/ui.config';
 import { validate } from './helpers/validate';
 import { showInputError, removeInputError } from './views/form';
+import { login } from './services/auth.service';
+import { notify } from './views/notifications';
 
 const { form, inputEmail, inputPassword } = UI;
 const inputs = [inputEmail, inputPassword];
 
 // Events
 form.addEventListener('submit', e => {
-    e.preventDefault();
-    onSubmit();
-})
-
-inputs.forEach(el => el.addEventListener('focus', () => removeInputError(el)))
+  e.preventDefault();
+  onSubmit();
+});
+inputs.forEach(el => el.addEventListener('focus', () => removeInputError(el)));
 
 // Handlers
-function onSubmit() {
-    const isValidForm = inputs.every((el) => {
-      const isValidInput = validate(el);
+async function onSubmit() {
+  const isValidForm = inputs.every(el => {
+    const isValidInput = validate(el);
+    if (!isValidInput) {
+      showInputError(el);
+    }
+    return isValidInput;
+  });
 
-      if (!isValidInput) {
-        showInputError(el);
-      }
+  if (!isValidForm) return;
 
-      return isValidInput;
-    })
-
-
+  try {
+    await login(inputEmail.value, inputPassword.value);
+    form.reset();
+    notify({ msg: 'Login success', className: 'alert-success' });
+  } catch (err) {
+    notify({ mas: 'Login faild', className: 'alert-danger' });
+  }
 }
