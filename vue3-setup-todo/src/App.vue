dca7740afb974b6127f862bcfa1244e31f151a13
DanMir0 <95764144+DanMir0@users.noreply.github.com>
2024-01-18 14:07:48 +0300

    Create Project & add axios get posts

---
+++ vue3-setup-todo/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import LearnPage from "./pages/LearnPage.vue";
+
+
+</script>
+
+<template>
+<learn-page></learn-page>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue3-setup-todo/src/pages/LearnPage.vue
@@ -0,0 +1,28 @@
+<script setup>
+import axios from 'axios'
+import { onMounted, ref } from 'vue'
+
+const posts = ref([])
+
+const getPosts = async () => {
+    try {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts/')
+        posts.value = response.data
+    } catch (e) {
+        console.log('response', e);
+    }
+}
+
+onMounted(getPosts)
+</script>
+
+<template>
+    <div class="post" v-for="post in posts" :key="post.id">
+        <h2>{{ post.title }}</h2>
+        <p>{{ post.body }}</p>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
