9e46111466aa0d98d23e19bc7abebf168cc0dc91
DanMir0 <95764144+DanMir0@users.noreply.github.com>
2024-01-18 18:55:24 +0300

    add form & add create post

--- vue3-setup-todo/src/App.vue
+++ vue3-setup-todo/src/App.vue
@@ -5,16 +5,14 @@ import LearnPage from "./pages/LearnPage.vue";
 </script>
 
 <template>
+
 <learn-page></learn-page>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
 }
 </style>
--- vue3-setup-todo/src/pages/LearnPage.vue
+++ vue3-setup-todo/src/pages/LearnPage.vue
@@ -3,6 +3,8 @@ import axios from 'axios'
 import { onMounted, ref } from 'vue'
 
 const posts = ref([])
+const title = ref('')
+const body = ref('')
 
 const getPosts = async () => {
     try {
@@ -13,16 +15,53 @@ const getPosts = async () => {
     }
 }
 
+function createPost() {
+    if (title.value === '') { 
+        return alert('Title is empty')
+    }
+    else {
+        posts.value.push({id: Date.now, title: title.value, body: body.value})
+    alert('Post add')}
+}
+
 onMounted(getPosts)
 </script>
 
 <template>
+    <div class="container">
+        <div style="display: flex; justify-content: center">
+            <form class="form">
+        <input type="text" v-model.trim="title" placeholder="title">
+        <input type="text" v-model.trim="body" placeholder="body">
+        <button @click.prevent="createPost">Add post</button>
+    </form>
+        </div>
     <div class="post" v-for="post in posts" :key="post.id">
         <h2>{{ post.title }}</h2>
         <p>{{ post.body }}</p>
     </div>
+    </div>
+    
 </template>
 
-<style lang="scss" scoped>
-
+<style scoped>
+.container {
+    margin: 0 auto;
+    width: 100%;
+    max-width: 1200px;
+}
+.form {
+    width: 50%;
+    margin: 20px 0;
+    padding: 30px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: #F8F8FF;
+}
+input {
+    width: 80%;
+    padding: 15px;
+    border-radius: 5%;
+}
 </style>
