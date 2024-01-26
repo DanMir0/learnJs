afd826379f4dcec8c2aaf9f4f5e7a521d7014864
DanMir0 <95764144+DanMir0@users.noreply.github.com>
2024-01-26 19:18:06 +0300

    Add animation

--- vue3-setup-todo/src/components/PostsList.vue
+++ vue3-setup-todo/src/components/PostsList.vue
@@ -1,5 +1,6 @@
 <script setup>
 import PostItem from '@/components/PostItem.vue';
+import { gsap } from "gsap";
 
 const props = defineProps({
     posts: {
@@ -7,11 +8,37 @@ const props = defineProps({
     require: true,
     }
 })
+function onBeforeEnter(el) {
+  el.style.opacity = 0
+}
+
+function onEnter(el, done) {
+  gsap.to(el, {
+    opacity: 1,
+    delay: el.dataset.index * 0.15,
+    onComplete: done
+  })
+}
+
+function onLeave(el, done) {
+  gsap.to(el, {
+    opacity: 0,
+    delay: el.dataset.index * 0.15,
+    onComplete: done
+  })
+}
 </script>
 
 <template>
     <div v-if="posts.length > 0">
-        <post-item :post="post" v-for="post in posts" :key="post.id" @remove="$emit('remove', post)"></post-item>
+        <transition-group 
+        name="list"
+        @before-enter="onBeforeEnter"
+        @enter="onEnter"
+        @leave="onLeave"
+        >
+            <post-item :post="post" v-for="post in posts" :key="post.id" @remove="$emit('remove', post)"></post-item>
+        </transition-group>
     </div>
     <div v-else>
         <h3>Список пуст</h3>
@@ -19,5 +46,9 @@ const props = defineProps({
 </template>
 
 <style scoped>
+.list-move {
+  transition: all 0.5s ease;
+}
+
 
 </style>
