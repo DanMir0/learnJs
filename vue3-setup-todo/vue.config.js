f19cfa4c6a158217500f6ffa1337ffc90f902987
DanMir0 <95764144+DanMir0@users.noreply.github.com>
2024-02-06 12:40:14 +0300

    todo

--- vue3-setup-todo/vue.config.js
+++ vue3-setup-todo/vue.config.js
@@ -1,6 +1,5 @@
-const { defineConfig } = require('@vue/cli-service');
-
-module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: '/learnJs/vue3-setup-todo/dist/'
-});
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+      ? '/learnJs/vue3-setup-todo/dist/'
+      : '/',
+};
