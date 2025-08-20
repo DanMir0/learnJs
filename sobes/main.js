89813d62c44a5e641b4b077c6dd72f4b2e936342
DanMir0 <95764144+DanMir0@users.noreply.github.com>
2025-08-20 22:30:30 +0300

    task for sobes

---
+++ sobes/main.js
@@ -0,0 +1,77 @@
+/* Задача 1 */
+// for (let i = 1; i <= 100; i++) {
+//     console.log(i);
+    
+// }
+
+/* Задача 2 */
+// function getLength(str) {
+//     return str.length
+// }
+// console.log(getLength("Привет"));
+
+/* Задача 3 */
+// function isEven(num) {
+//     return num % 2 == 0
+// }
+// console.log(isEven(2));
+
+/* Задача 4 */
+// function factorial(num) {
+//     if (num == 1) {
+//         return num
+//     } else {
+//        return num * factorial( num - 1)
+//     }
+    
+// }
+// console.log(factorial(5));
+
+/* Задача 5 */
+// function strReverse(str) {
+//     return str.split("").reverse().join("")
+// }
+// console.log(strReverse("hello"));
+
+/* Задача 6 */
+// function stringOfWords(str) {
+//     return str.split(" ")
+// }
+// console.log(stringOfWords("JavaScript is cool"));
+
+/* Задача 7 */
+// function deleteFirstElement(arr) {
+//     arr.shift()
+//     return arr
+// }
+// console.log(deleteFirstElement([1, 2, 3, 4, 5]));
+
+/* Задача 8 */
+// function deleteLastElement(arr) {
+//     arr.pop()
+//     return arr
+// }
+// console.log(deleteLastElement([1, 2, 3, 4, 5]));
+
+/* Задача 9 */
+// let arr = [1, 2, 3]
+// arr.push(4)
+// console.log(arr);
+
+/* Задача 10 */
+// let arr = [1, 2, 3]
+// arr.unshift(0)
+// console.log(arr);
+
+/* Задача 11 */
+// function sumElem(arr) {
+//     return arr.reduce((acc, elem) => acc + elem, 0)
+// }
+// console.log(sumElem([1, 2, 3, 4, 5]));
+
+/* Задача 12 */
+function multiArr(arr) {
+    let newArr = arr.map(item => item * 2)
+    return newArr
+}
+console.log(multiArr([1,2,3]));
