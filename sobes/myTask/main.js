443afd6bc13b9f1c3cdee7f871650dbfc81eca34
DanMir0 <95764144+DanMir0@users.noreply.github.com>
2025-08-27 22:18:30 +0300

    tasks for sobes

---
+++ sobes/myTask/main.js
@@ -0,0 +1,80 @@
+//! Напиши функцию flatten(arr), которая превращает вложенные массивы в плоский.
+// function flatten(arr) {
+// //? 1 способ
+// //      return arr.flat(Infinity)
+
+//     //? 2 способ
+//     // let newArr = []
+//     // for (let i = 0; i < arr.length; i++) {
+//     //     if (Array.isArray(arr[i])) {
+//     //         let newArr2 = flatten(arr[i]) 
+//     //         newArr = newArr.concat(newArr2)
+//     //     } else {
+//     //         newArr.push(arr[i])
+//     //     }
+//     // }
+//     // return newArr
+    
+//  //? 3 способ
+//     return arr.reduce((acc, val) => acc.concat(Array.isArray(val) ? flatten(val) : val), [])
+// }
+// console.log(flatten([1, [2, [3, 4]], 5]));
+
+//! Реализуй функцию debounce(fn, delay), которая вызывает функцию fn не чаще, чем раз в delay миллисекунд, даже если её вызывают много раз подряд.
+// function debounce(cb, delay = 1000, immediate = false) {
+//     let timer = null
+
+//     return (...args) => {
+//         let isCall = immediate && !timer
+
+//         clearTimeout(timer)
+
+//         timer = setTimeout(() => {
+//            if (!immediate) cb(...args)
+//         }, delay)
+
+//         if (isCall) cb(...args)
+//     }
+// }
+// const a = debounce(() => console.log("hi"), 3000, true);
+// a(); 
+// a()
+
+// function deepClone(obj) {
+//    if (obj === null || typeof obj !== 'object') return obj
+
+//    if (Array.isArray(obj)) {
+//     return obj.map(item => deepClone(item))
+//    }
+
+//    let copy = {}
+//    for (let key in obj) {
+//     copy[key] = deepClone(obj[key])
+//    }
+//    return copy
+// }
+// let obj = {name: 'Alex', age: 22, skills: {junior: ['html', 'css'], middle: 'A'}}
+// let copy = deepClone(obj)
+// obj.skills.junior[0] = 'asad'
+// console.log(obj);
+// console.log(copy);
+
+function once(fn) {
+    let isCompleted = false
+    let res = null
+    return (...args) => {
+        if (!isCompleted) {
+            isCompleted = true
+            res = fn(...args)
+        } 
+        return res
+    }
+}
+
+const f = once((x) => x * 2);
+const a = once((x) => x * 2);
+console.log(f(2)); // 4
+console.log(f(5)); // всё равно 4
+console.log(f(10)); // всё равно 4
+console.log(a(10)); 
+console.log(a(5)); 
